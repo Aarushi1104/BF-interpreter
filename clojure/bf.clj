3a5d1a670021078a173734cce71f9d8d9b36b20b
Max Besley <besleymax@gmail.com>
2024-07-03 23:50:31 +1000

    did everything

--- clojure/bf.clj
+++ clojure/bf.clj
@@ -1,24 +1,44 @@
 (ns bf.interpreter
   (:require [clojure.string :as str]))
 
+;; pointer means "memory/data pointer"
+(def init-pointer 0)
+(def tape-size 30)
+(def init-tape (vec (repeat tape-size 0)))
+
 (defn read-file [path]
-  (slurp path))
+  (-> path
+      slurp
+      str/trim-newline))
+
+(defn inc-at [index v]
+  (let [elem (get v index)]
+    (assoc v index (inc elem))))
+
+(defn dec-at [index v]
+  (let [elem (get v index)]
+    (assoc v index (dec elem))))
 
 (defn interpret [code tape ptr]
-  (let [tokens (seq code)
+  (let [cell (first tape)
+        tokens (seq code)
         token (first tokens)
-        rest-tokens (rest tokens)
-        head (first tape)]
-    (cond
-      (empty? tokens) tape
-      (empty? tape) (interpret code [0] ptr)
-      (= \+ token) (interpret rest-tokens (cons (+ 1 head) '(rest tape)) ptr)
-      (= \- token) (interpret rest-tokens (cons (- 1 head) '(rest tape)) ptr)
-      (= \> token) (interpret rest-tokens tape (inc ptr))
-      (= \< token) (interpret rest-tokens tape (dec ptr))
-      :else        (interpret rest-tokens tape ptr)))
-  )
+        rest-tokens (rest tokens)]
+    (if (empty? tokens) tape
+      (case token
+        \+    (interpret rest-tokens (inc-at ptr tape) ptr)
+        \-    (interpret rest-tokens (dec-at ptr tape) ptr)
+        \>    (interpret rest-tokens tape (inc ptr))
+        \<    (interpret rest-tokens tape (dec ptr))
+        \.    nil
+              ; otherwise
+              (interpret rest-tokens tape ptr)))))
 
 (defn bf [path]
-  (let [bf-code (str/trim-newline (read-file path))]
-    (interpret bf-code [0] 0)))
+  (let [bf-code (read-file path)]
+    (interpret bf-code init-tape init-pointer)))
+
+; testing
+; (prn (inc-at 0 [1 2 3]))
+; (prn (dec-at 2 [0 0 1]))
+(-> "./test.bf" bf prn)
